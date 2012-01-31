bd31eaa708961928c663b88c2a11892784130644
Azaya Studio <mauri.torchio@gmail.com>
2012-01-31 15:16:42 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+First::Application.config.secret_token = '83a685e979efd3b8a4fee55a0164ff66a9d51b5b50ef8b88c103383824df54ddafa11ec7f5cab35f9216cbf0b97daeb4ba144d3835de09015902f31e38dbfa9a'
